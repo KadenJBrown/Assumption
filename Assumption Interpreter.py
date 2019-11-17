ff8c4e6fe6447b6701dc8a0f31b893a803b81be1
KadenB <kbrown3388@gmail.com>
2019-11-16 22:47:04 -0800

    Started addition

--- Assumption Interpreter.py
+++ Assumption Interpreter.py
@@ -1,3 +1,12 @@
+# i - int - 1
+# f - float - 1.0
+# b - bool - True False
+## l - list - []
+## d - dict - {}
+## f - function - call()
+## s - scope - if else
+## g - gate - or and
+
 while True:
     filepath = input(">>> What file do you want to interpret?\n> ")
     print("\n>>> Interpreting file: " + filepath)
@@ -9,16 +18,29 @@ while True:
             for line in fp.readlines():
                 linenum += 1
                 if not line.startswith("assume"):
+                    # ASSUME MISSING ERROR
                     print(">>> ERROR: COMMAND MISSING ON LINE #"+str(linenum))
                     break
+                elif "+" in line:
+                    # 1 + 2 = 1 with a 2 at the end
+                    arg1 = ""
+                    arg2 = ""
+                    
                 elif "=" in line and not "==" in line:
+                    # SETTING VARIABLES
                     before = True
                     characternum = 0
                     variablename = ""
                     value = ""
+                    quotecount = 0
                     for character in line:
                         characternum += 1
-                        if character == "=":
+                        if character == "#":
+                            break
+                        elif character == "\"" or character == "'":
+                            quotecount += 1
+                            continue
+                        elif character == "=":
                             before = False
                             if variablename == "":
                                 print(">>> ERROR: VARIABLE TO SET MISSING ON LINE #"+str(linenum))
@@ -28,7 +50,6 @@ while True:
                             variablename += character
                         elif character != " " and before == False:
                             value += character
-                        elif character == "#"
                     if value == "":
                         print(">>> ERROR: VALUE TO SET VARIABLE TO MISSING ON LINE #"+str(linenum))
                         error = True
@@ -39,26 +60,55 @@ while True:
                         value = float(value)
                     elif variablename.startswith("b"):
                         value = bool(value)
-                    elif variablename.startswith("l"):
-                        # LIST
-                    elif variablename.startswith("d"):
-                        # DICTIONARY
-                    elif not variablename.startswith("s"):
+                    elif variablename.startswith("s"):
+                        if quotecount != 2:
+                            print(">>> ERROR: VARIABLE SAYS VALUE IS A STRING, NOT A STRING AT LINE #"+str(linenum))
+                            break
+                    elif not variablename.startswith("l") and not variablename.startswith("d"):
                         print(">>> ERROR: TYPE OF VARIABLE UNKNOWN ON LINE #"+str(linenum))
                         break
+                    variables[variablename] = value
                 elif line.startswith("assume '") or line.startswith("assume \""):
+                    # PRINT STRING RAW
                     inside = False
                     result = ""
                     first = ""
+                    ignore = False
                     for character in line:
-                        if character == first:
+                        if ignore == False and character == first:
                             break
+                        ignore = False
+                        if character == "\\":
+                            ignore = True 
                         elif inside:
                             result += character
                         elif first == "" and (character == "'" or character == "\""):
                             first = character
                             inside = True
                     print(result)
+                elif line.startswith("assume s"):
+                    # PRINT STRING VAR
+                    characternum = 0
+                    variablename = ""
+                    for character in line:
+                        characternum += 1
+                        if character == "\n":
+                            continue
+                        elif characternum > 7:
+                            variablename += character
+                    print(variables[variablename])
+                else:
+                    line_no_comment = ""
+                    for character in line:
+                        if character == "#":
+                            break
+                        else:
+                            line_no_comment += character
+                    if line_no_comment == "assume":
+                        continue
+                    else:
+                        print(">>> ERROR: UNKNOWN LINE FOR LINE #"+str(linenum))
+                        break
                 if error:
                     break
     else:
